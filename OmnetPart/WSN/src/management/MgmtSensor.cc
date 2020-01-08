49cec6ea8ebe9037a336e46d346a3b6df91c9c75
JaevillenOliveira <jaevillenks@gmail.com>
2020-01-08 12:10:56 -0300

    Adapt the code for hard-handover instead of soft-handover

--- OmnetPart/WSN/src/management/MgmtSensor.cc
+++ OmnetPart/WSN/src/management/MgmtSensor.cc
@@ -60,47 +60,54 @@ void MgmtSensor::processScanCommand(Ieee80211Prim_ScanRequest *ctrl)
 {
     EV << "Received Scan Request from agent, clearing AP list and starting scanning...\n";
 
-    if (!isScanning){
-//            throw cRuntimeError("processScanCommand: scanning already in progress");
+    if (isScanning)
+       throw cRuntimeError("processScanCommand: scanning already in progress");
 
-        //COMMENTED BY JAEVILLEN
-//        if (mib->bssStationData.isAssociated)
-//            disassociate();
-
-        if (assocTimeoutMsg) {
-            EV << "Cancelling ongoing association process\n";
-            delete cancelEvent(assocTimeoutMsg);
-            assocTimeoutMsg = nullptr;
-        }
-
-        // clear existing AP list (and cancel any pending authentications) -- we want to start with a clean page
-        clearAPList();
-
-        // fill in scanning state
-        ASSERT(ctrl->getBSSType() == BSSTYPE_INFRASTRUCTURE);
-        scanning.bssid = ctrl->getBSSID().isUnspecified() ? MacAddress::BROADCAST_ADDRESS : ctrl->getBSSID();
-        scanning.ssid = ctrl->getSSID();
-        scanning.activeScan = ctrl->getActiveScan();
-        scanning.probeDelay = ctrl->getProbeDelay();
-        scanning.channelList.clear();
-        scanning.minChannelTime = ctrl->getMinChannelTime();
-        scanning.maxChannelTime = ctrl->getMaxChannelTime();
-        ASSERT(scanning.minChannelTime <= scanning.maxChannelTime);
-
-        // channel list to scan (default: all channels)
-        for (size_t i = 0; i < ctrl->getChannelListArraySize(); i++)
-            scanning.channelList.push_back(ctrl->getChannelList(i));
-        if (scanning.channelList.empty())
-            for (int i = 0; i < numChannels; i++)
-                scanning.channelList.push_back(i);
-
-        // start scanning
-        if (scanning.activeScan)
-            host->subscribe(IRadio::receptionStateChangedSignal, this);
-        scanning.currentChannelIndex = -1;    // so we'll start with index==0
-        isScanning = true;
-        scanNextChannel();
+    if (mib->bssStationData.isAssociated){
+        //disassociate(); COMMENTED BY JAEVILLEN
+        //ADDED BY JAEVILLEN
+        /*In the original code the station doesn't notify the AP that it's breaking the association
+         * and, therefore the AP can't register the correct number of sensors that are associated */
+        Ieee80211Prim_DisassociateRequest *req = new Ieee80211Prim_DisassociateRequest();
+        req->setAddress(assocAP.address);
+        req->setReasonCode(RC_UNSPECIFIED);
+        processDisassociateCommand(req);
     }
+
+    if (assocTimeoutMsg) {
+        EV << "Cancelling ongoing association process\n";
+        delete cancelEvent(assocTimeoutMsg);
+        assocTimeoutMsg = nullptr;
+    }
+
+    // clear existing AP list (and cancel any pending authentications) -- we want to start with a clean page
+    clearAPList();
+
+    // fill in scanning state
+    ASSERT(ctrl->getBSSType() == BSSTYPE_INFRASTRUCTURE);
+    scanning.bssid = ctrl->getBSSID().isUnspecified() ? MacAddress::BROADCAST_ADDRESS : ctrl->getBSSID();
+    scanning.ssid = ctrl->getSSID();
+    scanning.activeScan = ctrl->getActiveScan();
+    scanning.probeDelay = ctrl->getProbeDelay();
+    scanning.channelList.clear();
+    scanning.minChannelTime = ctrl->getMinChannelTime();
+    scanning.maxChannelTime = ctrl->getMaxChannelTime();
+    ASSERT(scanning.minChannelTime <= scanning.maxChannelTime);
+
+    // channel list to scan (default: all channels)
+    for (size_t i = 0; i < ctrl->getChannelListArraySize(); i++)
+        scanning.channelList.push_back(ctrl->getChannelList(i));
+    if (scanning.channelList.empty())
+        for (int i = 0; i < numChannels; i++)
+            scanning.channelList.push_back(i);
+
+    // start scanning
+    if (scanning.activeScan)
+        host->subscribe(IRadio::receptionStateChangedSignal, this);
+    scanning.currentChannelIndex = -1;    // so we'll start with index==0
+    isScanning = true;
+    scanNextChannel();
+
 }
 
 void MgmtSensor::startAssociation(ApInfo *ap, simtime_t timeout)
@@ -159,19 +166,21 @@ void MgmtSensor::handleAssociationResponseFrame(Packet *packet, const Ptr<const 
 
     if (mib->bssStationData.isAssociated) {
         EV << "Breaking existing association with AP address=" << assocAP.address << "\n";
-        //ADDED BY JAEVILLEN: BEGIN
-        Ieee80211Prim_DisassociateRequest *req = new Ieee80211Prim_DisassociateRequest();
-        req->setAddress(assocAP.address);
-        req->setReasonCode(RC_UNSPECIFIED);
-        processDisassociateCommand(req);
-        //ADDED BY JAEVILLEN: END
-
-//COMMENTED BY JAEVILLEN
-//        mib->bssStationData.isAssociated = false;
-//        delete cancelEvent(assocAP.beaconTimeoutMsg);
-//        assocAP.beaconTimeoutMsg = nullptr;
-//        assocAP = AssociatedApInfo();
+        mib->bssStationData.isAssociated = false;
+        delete cancelEvent(assocAP.beaconTimeoutMsg);
+        assocAP.beaconTimeoutMsg = nullptr;
+        assocAP = AssociatedApInfo();
     }
+//        //ADDED BY JAEVILLEN: BEGIN
+//    if (mib->bssStationData.isAssociated) {
+//        EV << "Breaking existing association with AP address=" << assocAP.address << "\n";
+//        Ieee80211Prim_DisassociateRequest *req = new Ieee80211Prim_DisassociateRequest();
+//        req->setAddress(assocAP.address);
+//        req->setReasonCode(RC_UNSPECIFIED);
+//        processDisassociateCommand(req);
+//    }
+//        //ADDED BY JAEVILLEN: END
+
 
     delete cancelEvent(assocTimeoutMsg);
     assocTimeoutMsg = nullptr;
---
+++ OmnetPart/WSN/src/networkcontroller/ControllerInterface.cc
@@ -0,0 +1,148 @@
+/*
+ * cMatlabInterface.cc
+ *
+ *  Created on: Jul 18, 2019
+ *      Author: jaevillen
+ */
+
+#include <string.h>
+#include <omnetpp.h>
+#include <stdio.h>
+#include <iostream>
+#include "ControllerInterface.h"
+#include "../management/ApReport.h"
+
+
+using namespace std;
+using std::endl;
+using namespace omnetpp;
+
+#define ON                        1
+#define OFF                       0
+
+Define_Module(ControllerInterface);
+Register_Class(ControllerInterface::ApInfo);
+
+ControllerInterface::~ControllerInterface() {
+    cancelAndDelete(apSortingTimer);
+}
+
+void ControllerInterface::initialize()
+{
+    this->getSimulation()->getSystemModule()->subscribe("reportReadySignal", this);
+    apSortingTimer = new cMessage("apSortingTimer");
+    scheduleAt(simTime() + 2, apSortingTimer);
+    //p = new OMNeTPipe("localhost", 18638);
+
+};
+
+void ControllerInterface::handleMessage(cMessage *msg)
+{
+    if (msg == apSortingTimer) {
+        if(!aplist.empty()){
+            vec.clear();
+            std::copy(aplist.begin(),aplist.end(),std::back_inserter<std::vector<pair>>(vec)); //copies items from map to vector
+
+            std::sort(vec.begin(), vec.end(), [](const pair& l, const pair& r) { //sorts vector by throughput averages
+                if (l.second.getCurrentReport().getThroughput() != r.second.getCurrentReport().getThroughput())
+                    return l.second.getCurrentReport().getThroughput() < r.second.getCurrentReport().getThroughput();
+            });
+//            for (auto const &pair: vec) {
+//                cout <<  pair.first;
+//                printf("\n");
+//            }
+            for (auto it = vec.begin(); it != vec.end(); ++it) {
+                //apAnalisys(it->first, it->second);
+            }
+            scheduleAt(simTime() + 10, apSortingTimer);
+        }
+    }
+}
+
+ControllerInterface::ApInfo *ControllerInterface::lookupAp(int id) {
+    auto it = aplist.find(id);
+    return it == aplist.end() ? nullptr : &(it->second);
+}
+
+simsignal_t ControllerInterface::concatRegister(std::string s, std::string s1){
+        s += s1;
+        int n = s.length();
+        char char_array[n + 1];
+        strcpy(char_array, s.c_str());
+        const char *signalName = char_array;
+        simsignal_t signalID = registerSignal(signalName);
+        return signalID;
+}
+
+void ControllerInterface::receiveSignal (cComponent *source, simsignal_t signalID, cObject *obj, cObject *details)
+{
+    Enter_Method_Silent();
+    ApReport *report;
+    ApInfo *ap = lookupAp(source->getId());
+    if(!ap){
+        printf("\n %s %i %s ", "AP", source->getId(), "now is in the list ");
+        ap = &aplist[source->getId()];
+        ap->setControlSignalID(concatRegister(s, std::to_string(source->getId())));
+        ap->setOff(false);
+    }
+    report = static_cast<ApReport*> (obj);
+    ap->setCurrentReport(*report);
+
+    printf("\n");
+    cout << "ID: " << source->getId() << ("    ");
+    cout << "Number of sensors: " << report->getNumberOfSta() << ("    ");
+    cout << "Number of Neighbors: " << report->getNumberOfNeighbours() << ("    ");
+    cout << "RSSI: " << report->getRssiMean() << ("    ");
+    cout << "Throughput: " << report->getThroughput();
+}
+
+void ControllerInterface::apAnalisys (const int id, ApInfo &ap){
+
+    std::string s = "packet" + std::to_string(packetsCount++);
+    int n = s.length();
+    char char_array[n + 1];
+    strcpy(char_array, s.c_str());
+    char *packetName = char_array;
+
+    OMNeTPk* pk = new OMNeTPk(packetName);
+    pk->addVal("sensors", FLOAT(ap.getCurrentReport().getNumberOfSta()), TYPE_FLOAT);
+    pk->addVal("neighbors", FLOAT(ap.getCurrentReport().getNumberOfNeighbours()), TYPE_FLOAT);
+    pk->addVal("rssi", FLOAT(ap.getCurrentReport().getRssiMean()), TYPE_FLOAT);
+    pk->addVal("throughput", FLOAT(ap.getCurrentReport().getThroughput()), TYPE_FLOAT);
+    p->sendPk(*pk);
+
+    OMNeTPk* rpk = p->recvPk();
+    char* rName = rpk->getName(0);
+    void* r = rpk->getVal(rpk->getName(0));
+    float resp = *(float *)&r;
+    cout << rName << "  " << resp << endl;
+
+    if(resp <= 50 && ap.isOff()){
+        restartAp(id, &ap);
+    } else if (resp > 50 && !ap.isOff()){
+        shutdownAp(id, &ap);
+        cout << "Shutting down ap" << id << endl ;
+
+    }
+}
+
+void ControllerInterface::shutdownAp (const int id, ApInfo *ap){
+    printf("\n %s %i ", "Shutting Down AP", id);
+    ap->setOff(true);
+    this->emit(ap->getControlSignalID(),true, nullptr);
+}
+
+void ControllerInterface::restartAp (const int id, ApInfo *ap){
+    printf("\n %s %i", "Restarting AP", id);
+    this->emit(ap->getControlSignalID(),false, nullptr);
+    ap->setOff(false);
+}
+
+
+
+
+
+
+
+
+
