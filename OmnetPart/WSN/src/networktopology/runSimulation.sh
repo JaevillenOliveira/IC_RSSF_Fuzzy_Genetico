28b2629dc17099ba837a6d3fc7dcadd2c5aaf7e9
JaevillenOliveira <jaevillenks@gmail.com>
2020-03-09 08:32:37 -0300

    Fixing simulation script #1

--- OmnetPart/WSN/src/networktopology/runSimulation.sh
+++ OmnetPart/WSN/src/networktopology/runSimulation.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-gnome-terminal -x sh -c sudo /usr/local/MATLAB/R2018b/bin/matlab -nodisplay -nosplash -nodesktop -r "run('../../../../MatlabPart/Matlab_Bridge/Sever.m')"; bash
+
+#Starting Matlab Server
+path_to_server="../../../../MatlabPart/Matlab_Bridge/Sever.m"
+run_command="run('Server.m')"
+gnome-terminal --working-directory=IC/MatlabPart/Matlab_Bridge/ -- sudo /usr/local/MATLAB/R2018b/bin/matlab -nodisplay -nosplash -nodesktop -r ${run_command} &
+
 
 for i in $(seq 1 10)
 do	
-.././WSN -u Cmdenv -f wsn.ini -n ../../simulations:..:../../../../inet4/src:../../../../inet4/examples:../../../../inet4/tutorials:../../../../inet4/showcases -c wsnSc1T${i} --vector-recording=false
+sudo .././WSN -u Cmdenv -f wsn.ini -n ../../simulations:..:../../../../inet4/src:../../../../inet4/examples:../../../../inet4/tutorials:../../../../inet4/showcases -c wsnSc1T${i} --vector-recording=false
 done
 
 
-
+#gnome-terminal -x sh -c
