95eaa7b844d0a728bacdaf518de0b3b31d5bf3d2
JaevillenOliveira <jaevillenks@gmail.com>
2020-03-18 01:17:43 -0300

    Up

--- MatlabPart/Matlab_Bridge/Server.m
+++ MatlabPart/Matlab_Bridge/Server.m
@@ -2,29 +2,33 @@ fis = readfis('ComnetUniformSetsV1');
 
 pipe = OMNeTPipe("localhost", 18638);
 responseNames = 'respPk';
-for k=1:inf
-    [headerName, pktMap] = recvPk(pipe);
+while 1
+    try
+        [headerName, pktMap] = recvPk(pipe);
 
-    disp(headerName);
-    sensors = pktMap('sensors');
-    neighbors = pktMap('neighbors');
-    rssi = pktMap('rssi');
-    throughput = pktMap('throughput');
-    
-    fprintf("Sensors:");
-    disp(sensors)
-    fprintf("Neighbors:");
-    disp(neighbors)
-    fprintf("RSSI:");
-    disp(rssi)
-    fprintf("Throughput:");
-    disp(throughput)
-    
-    output = evalfis(fis,[rssi neighbors sensors throughput]);   
-    respName{k} = [responseNames '_' num2str(k,'%d')];
-    sendPk(pipe, 'fuzzyResp', output, pipe.typeFloat, respName{k});
-    disp(respName{k});
-    disp(output);
+        disp(headerName);
+        sensors = pktMap('sensors');
+        neighbors = pktMap('neighbors');
+        rssi = pktMap('rssi');
+        throughput = pktMap('throughput');
+
+        fprintf("Sensors:");
+        disp(sensors)
+        fprintf("Neighbors:");
+        disp(neighbors)
+        fprintf("RSSI:");
+        disp(rssi)
+        fprintf("Throughput:");
+        disp(throughput)
+
+        output = evalfis(fis,[rssi neighbors sensors throughput]);   
+        respName{k} = [responseNames '_' num2str(k,'%d')];
+        sendPk(pipe, 'fuzzyResp', output, pipe.typeFloat, respName{k});
+        disp(respName{k});
+        disp(output);
+    catch
+       % break;
+    end
 
 end
 
--- OmnetPart/WSN/src/networktopology/runSimulation.sh
+++ OmnetPart/WSN/src/networktopology/runSimulation.sh
@@ -4,7 +4,11 @@
 
 for i in $(seq 1 10)
 do	
+
 gnome-terminal --working-directory=IC/MatlabPart/Matlab_Bridge/ -- /usr/local/MATLAB/R2018b/bin/matlab -nodisplay -nosplash -nodesktop -r "run('Server.m')" &
-sleep 5
+
+sleep 20
+
 .././WSN -u Cmdenv -f wsn.ini -n ../../simulations:..:../../../../inet4/src:../../../../inet4/examples:../../../../inet4/tutorials:../../../../inet4/showcases -c wsnSc1T${i} --vector-recording=false
+
 done
