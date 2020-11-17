a68c59b2d23df767a613d5a9d51c140e9455ab70
JaevillenOliveira <jaevillenks@gmail.com>
2020-11-17 09:16:45 -0300

    updating refs on topology change

--- OmnetPart/WSN/src/networktopology/runSimulation.sh
+++ OmnetPart/WSN/src/networktopology/runSimulation.sh
@@ -9,8 +9,8 @@ sleep 12
 
 cd ~
 cd IC/OmnetPart/WSN/src/networktopology
-#./runSc1FLC.sh
-#./runSc2FLC.sh
+./runSc1FLC.sh
+./runSc2FLC.sh
 ./runSc3FLC.sh
 
 rm /home/jaevillen/IC/MatlabPart/Matlab_Bridge/TempSolution.fis
@@ -20,4 +20,3 @@ cd results
 
 python Filter.py
 
-# Call GA
